4b68f74a570e6759104abd7d39b9739a68adfafe
perejog04 <perejog04@gmail.com>
2024-03-28 22:51:44 +0300

    beta 0.1 done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
 "use strict"; // включаем строгий режим
+
+const btn = document.getElementById('toDo-btn');
+const inpt = document.getElementById('toDo-input');
+const willDoList = document.querySelector('.willDo-list');
+const wasDoingList = document.querySelector('.wasDoing-list');
+
+btn.addEventListener('click', () => {
+    willDoList.insertAdjacentHTML(
+        'beforeend',
+        `<div class="willDo-item">${inpt.value}</div>`
+    )
+    inpt.value = "";
+})
+
+inpt.addEventListener('keyup', (event) => {
+    if (event.code == 'Enter' && inpt.value > "") {
+        willDoList.insertAdjacentHTML(
+            'beforeend',
+            `<div class="willDo-item">${inpt.value}</div>`
+        )
+        inpt.value = "";
+    }
+})
+
+willDoList.addEventListener('click', (event) => {
+    if (event.target.closest('.willDo-item')) {
+        // const text = event.target.textContent;
+        wasDoingList.insertAdjacentHTML(
+            'afterbegin',
+            `<div class="wasDoing-item">${event.target.textContent}</div>`
+        )
+        event.target.remove();
+    }
+    
+})
+
+wasDoingList.addEventListener('click', (event) => {
+    if (event.target.closest('.wasDoing-item')) {
+        // const text = event.target.textContent;
+        willDoList.insertAdjacentHTML(
+            'afterbegin',
+            `<div class="willDo-item">${event.target.textContent}</div>`
+        )
+        event.target.remove();
+    }
+    
+})
+
+// document.addEventListener("keydown", (event) => {
+//     console.log(`${event.code} ${event.key}`);
+// })
